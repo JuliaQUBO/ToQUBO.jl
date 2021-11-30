78e271d6aeb651cdfed2bfe714cd09b99a4ae3f6
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2021-11-30 12:41:21 -0300

    Add Main Package File

--- src/ToQUBO.jl
+++ src/ToQUBO.jl
@@ -1,8 +1,21 @@
 module ToQUBO
+    using JuMP, MathOptInterface
+    const MOI = MathOptInterface
+    const MOIU = MathOptInterface.Utilities
+    const MOIB = MathOptInterface.Bridges
 
-using MathOptInterface
-using DataStructures
+    const SVF = MOI.SingleVariable
+    const VVF = MOI.VectorOfVariables
+    const SAF{T} = MOI.ScalarAffineFunction{T}
+    const VAF{T} = MOI.VectorAffineFunction{T}
+    const SQF{T} = MOI.ScalarQuadraticFunction{T}
+    const VQF{T} = MOI.VectorQuadraticFunction{T}
 
-include("moi.jl")
+    const VI = MOI.VariableIndex
+    const CI = MOI.ConstraintIndex
+
+    
+
+    include("moi.jl")
 
 end # module
