adacf8f33d8142ba6a6bfd686014205c7e39c631
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2021-12-02 07:52:56 -0300

    Include VI + SAF support

--- src/supported.jl
+++ src/supported.jl
@@ -1,14 +1,20 @@
-"""
-From https://github.com/jump-dev/Dualization.jl @ src/supported.jl
-"""
+# Objective Support
+function supported_objective(model::MOIU.Model{T}) where T
+    F = MOI.get(model, MOI.ObjectiveFunctionType())
+    if !__supported_objective(F)
+        error("Objective functions of type ", F, " are not implemented")
+    end
+    return
+end
 
-"""
-    supported_constraints(con_types::Vector{Tuple{DataType, DataType}})
-Returns `true` if `Function-in-Set` is supported for Dualization and throws an error if it is not.
-"""
-function supported_constraints(con_types::Vector{Tuple{DataType, DataType}})
-    for (F, S) in con_types
-        if !supported_constraint(F, S)
+__supported_objective(::Type) = false
+__supported_objective(::VI) = true
+__supported_objective(::SAF) = true
+
+# Constraint Support
+function supported_constraints(model::MOIU.Model{T}) where T
+    for (F, S) in MOI.get(model, MOI.ListOfConstraints())
+        if !__supported_constraint(F, S)
             error(
                 "Constraints of function ",
                 F,
@@ -21,36 +27,4 @@ function supported_constraints(con_types::Vector{Tuple{DataType, DataType}})
     return
 end
 
-supported_constraint(::Type, ::Type) = false
-
-function supported_constraint(
-    ::Type{<:Union{MOI.SingleVariable,MOI.ScalarAffineFunction}},
-    S::Type{<:MOI.AbstractScalarSet},
-)
-    return _dual_set_type(S) !== nothing
-end
-function supported_constraint(
-    ::Type{<:Union{MOI.VectorOfVariables,MOI.VectorAffineFunction}},
-    S::Type{<:MOI.AbstractVectorSet},
-)
-    return _dual_set_type(S) !== nothing
-end
-
-"""
-    supported_objective(primal_model::MOI.ModelLike)
-Returns `true` if `MOI.ObjectiveFunctionType()` is supported for Dualization and throws an error if it is not.
-"""
-function supported_objective(primal_model::MOI.ModelLike)
-    obj_func_type = MOI.get(primal_model, MOI.ObjectiveFunctionType())
-    if !supported_obj(obj_func_type)
-        error("Objective functions of type $obj_func_type are not implemented")
-    end
-    return
-end
-
-# General case
-supported_obj(::Type) = false
-# List of supported objective functions
-supported_obj(::Type{SVF}) = true
-supported_obj(::Type{<:SAF}) = true
-supported_obj(::Type{<:SQF}) = true
+__supported_constraint(::Type, ::Type) = false
