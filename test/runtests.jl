a7d16c06cfcd8ac2d49f99c38ca1173ddcf36386
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2021-12-02 15:23:00 -0300

    Add Posiform Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,15 @@
 using Test
 
 function tests()
-    @testset "MILP" begin
+    @testset "PosiformTest" begin
+        include("../src/posiform.jl")
 
+        ∅ = Vector{Int}()
+
+        p = Posiform{Int, Float64}([1] => 0.3, [1,2] => -0.5, [2,3] => 1.2)
+        q = Posiform{Int, Float64}([1] => -0.3, [1,2] => 2.5, [2,3] => 1.0, ∅ => 2.0)
+
+        @test (p + q) == Posiform{Int, Float64}([1, 2] => 2.0, [2, 3] => 2.2, ∅ => 2.0)
     end
 end
 
