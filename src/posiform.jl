c6368203f9568d7e567ec882f5a1090ec925d64e
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2021-12-02 15:22:40 -0300

    Enhance Posiform Operator Coverage

--- src/posiform.jl
+++ src/posiform.jl
@@ -68,12 +68,32 @@ function Base.keys(p::Posiform)
     return keys(p.pairs)
 end
 
+function Base.iterate(p::Posiform{S, T}) where {S, T}
+    return iterate(p.pairs)
+end
+
+function Base.iterate(p::Posiform{S, T}, n::Int) where {S, T}
+    return iterate(p.pairs, n)
+end
+
 function Base.getindex(p::Posiform{S, T}, k::Set{S}) where {S, T}
-    return getindex(p.pairs, k)
+    return get(p.pairs, k, T(0))
+end
+
+function Base.getindex(p::Posiform{S, T}, k::S) where {S, T}
+    return get(p.pairs, Set{S}([k]), T(0))
 end
 
 function Base.get(p::Posiform{S, T}, k::Set{S}, d::T) where {S, T}
     return get(p.pairs, k, d)
+end
+
+function Base.setindex!(p::Posiform{S, T}, value::T, key::Set{S}) where {S, T}
+    setindex!(p.pairs, value, key)
+end
+
+function Base.setindex!(p::Posiform{S, T}, value::T, key::S) where {S, T}
+    setindex!(p.pairs, value, Set{S}([key]))
 end
 
 function vars(p::Posiform{S, T}) where {S, T}
@@ -92,7 +112,11 @@ function toqubo(p::Posiform{S, T}) where {S, T}
 end
 
 function Base.copy(p::Posiform{S, T}) where {S, T}
-    return Posiform{S, T}(copy(p.pairs))
+    if isempty(p)
+        return Posiform{S, T}()
+    else
+        return Posiform{S, T}(copy(p.pairs))
+    end
 end
 
 function Base.isempty(p::Posiform)
@@ -108,6 +132,10 @@ function Base.:+(p::Posiform{S, T}, q::Posiform{S, T}) where {S, T}
 end
 
 function Base.:+(p::Posiform{S, T}, c::T) where {S, T}
+    if c == T(0)
+        return copy(p)
+    end
+
     q = copy(p)
     ∅ = Set{S}()
     q.pairs[∅] = get(q.pairs, ∅, T(0)) + c
@@ -131,6 +159,24 @@ function Base.:-(p::Posiform{S, T}, c::T) where {S, T}
     return q
 end
 
+function Base.:*(p::Posiform{S, T}, q::Posiform{S, T}) where {S, T}
+    if isempty(p) || isempty(q)
+        return Posiform{S, T}()
+    end
+
+    r = Posiform{S, T}()
+
+    for (pₖ, pᵥ) in p.pairs, (qₖ, qᵥ) in q.pairs
+        rₖ = union(pₖ, qₖ)
+        rᵥ = pᵥ * qᵥ
+        if rᵥ != T(0)
+            r[rₖ] += rᵥ
+        end
+    end
+
+    return r
+end
+
 function Base.:*(p::Posiform{S, T}, c::T) where {S, T}
     if c == 0
         return Posiform{S, T}()
@@ -147,16 +193,18 @@ end
 
 Base.:*(c::T, p::Posiform{S, T}) where {S, T} = (p * c)
 
+function Base.:^(p::Posiform{S, T}, n::Int) where {S, T}
+    q = Posiform{S, T}(1.0)
+
+    for i=1:n
+        q = q * p
+    end
+
+    return q
+end
+
 function subscript(i::Int)
-    if i < 0
-        c = [Char(0x208B)]
-    else
-        c = []
-    end
-    for d in reverse(digits(abs(i)))
-        push!(c, Char(0x2080+d))
-    end
-    return join(c)
+    return join([(i < 0) ? Char(0x208B) : ""; [Char(0x2080 + j) for j in reverse(digits(abs(i)))]])
 end
 
 function Base.print(io::IO, p::Posiform{T}) where {T}
