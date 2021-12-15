6a5ade250abe74dcd4bb9cfd632880d3151e126f
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2021-12-15 16:24:50 -0300

    Add QUBO Model Struct

--- src/qubomodel.jl
+++ src/qubomodel.jl
@@ -1,63 +1,4 @@
-@doc raw"""
-
-
-tech:
-
-    :bin - Binary expansion i.e. $ y = \sum_{i = 1}^{n} 2^{i-1} x_i $
-    :step - Step expansion i.e. $ y = \sum_{i = 1}^{n} x_i $
-"""
-struct VirtualVar{S <: Any, T <: Any}
-
-    bits::Int
-    vars::Vector{S}
-    tech::Symbol
-    offset::Int 
-    slack::Bool 
-    values::Vector{Union{T, Nothing}}
-
-    function VirtualVar{T}(bits::Int, vars::Vector{S}; tech::Symbol=:bin, offset::Int=0, slack::Bool=false) where T
-        
-        if length(vars) != bits
-            error("Virtual Variables need exactly as many keys as encoding bits")
-        end
-
-        if !(tech === :bin || tech === :step)
-            error("Invalid Expansion technique '$tech'")
-        end
-
-        values = [nothing for _ in 1:bits]
-
-        return new{T}(bits, vars, tech, offset, slack, values)
-    end
-end
-
-function coefficients(v::VirtualVar{S, T})::Vector{Tuple{S, T}} where {S, T}
-    if v.tech === :step
-        a = convert(T, 1)
-        return [(v.vars[i], a) for i in 1:v.bits]
-    elseif v.tech === :bin
-        a = convert(T, 2)
-        return [(v.vars[i], a ^ (i - v.offset - 1)) for i in 1:v.bits]
-    end
-end
-
-function vars(v::VirtualVar)
-    return v.vars
-end
-
-"""
-"""
-mutable struct QUBOModel{T <: Any} <: MOIU.AbstractModelLike{T}
-
-    model::MOIU.Model{T}
-    varmap::Dict{VI, VirtualVar}
-    quantum::Bool
-
-    function QUBOModel{T}(; quantum::Bool=false) where T
-        return new{T}(MOIU.Model{T}(), MOIU.IndexMap(), quantum)
-    end
-end
-
+# -*- Constants -*-
 const MOIObjective = Union{
     MOI.ObjectiveFunctionType,
     MOI.ObjectiveSense,
@@ -72,60 +13,69 @@ const MOIConstraints = Union{
     MOI.ListOfConstraintTypesPresent
 }
 
-function MOI.get(model::QUBOModel, attr::MOIObjective)
-    return MOI.get(model.model, attr)
-end
+"""
+"""
+mutable struct QUBOModel{T <: Any} <: MOIU.AbstractModelLike{T}
 
-function MOI.get(model::QUBOModel, attr::MOIVariables)
-    return MOI.get(model.model, attr)
-end
+    model::MOIU.Model{T}
+    varmap::Dict{VI, VirtualVar}
+    virmap::Dict{VI, VirtualVar}
+    varvec::Vector{VirtualVar}
+    quantum::Bool
 
-function MOI.get(model::QUBOModel, attr::MOIConstraints)
-    return MOI.get(model.model, attr)
-end
+    function QUBOModel{T}(; quantum::Bool=false) where T
 
-function MOI.set(model::QUBOModel, attr::MOI.ObjectiveSense, sense::MOI.OptimizationSense)
-    return MOI.set(model.model, attr, sense)
-end
-
-function value(model::QUBOModel{T}, v::VirtualVar{T}) where T
-    s = convert(T, 0)
-    for (cᵢ, vᵢ) in expand(v)
-        xᵢ = MOI.get(model, MOI.VariablePrimal(), vᵢ)
-        s += cᵢ * xᵢ
+        model = MOIU.Model{T}()
+        varmap = Dict{VI, VirtualVar}()
+        virmap = Dict{VI, VirtualVar}()
+        varvec = Vector{VirtualVar}()
+        return new{T}(model, varmap, virmap, varvec, quantum)
     end
-    return s
 end
 
-function addvar(bits::Int, model::QUBOModel{T}; slack::Bool=false)::VirtualVar{T} where T
+function addvar(model::QUBOModel{T}, bits::Int; offset::Int=0, slack::Bool=false)::VirtualVar{VI, T} where T
 
-    vars = MOI.add_variables(model, bits)
+    vars = MOI.add_variables(model.model, bits)
 
-    v = VirtualVar(bits, vars, base=2, offset=0, slack=slack)
+    v = VirtualVar{VI, T}(bits, vars, tech=:bin, offset=offset, slack=slack)
 
     for vᵢ in vars
-        MOI.add_constraint(model, vᵢ, MOI.ZeroOne())
-        model.varmap[vᵢ] = v
+        MOI.add_constraint(model.model, vᵢ, MOI.ZeroOne())
+        model.virmap[vᵢ] = v
     end
+
+    push!(model.varvec, v)
 
     return v
 end
 
 """
 """
-function addslack(model::QUBOModel{T}, c::T)::VirtualVar{T} where T
-    bits = ndigits(ceil(Int, log(2, c)), base=2)
-    return addvar(model, bits)
+function addslack(model::QUBOModel{T}, bits::Int)::VirtualVar{VI, T} where T
+    return addvar(model, bits, slack=true)
 end
 
 """
 """
-function addslack(bits::Int, model::QUBOModel{T})::VirtualVar{T} where T
-    return addvar(bits, model, slack=true)
+function expand(model::QUBOModel{T}, var::VI, bits::Int; offset::Int=0)::VirtualVar{VI, T} where T
+    model.varmap[var] = addvar(model, bits, offset=offset)
+    return model.varmap[var]
 end
 
 """
 """
-function expand()
+function isqubo(model::QUBOModel)::Bool
+    return isqubo(model.model)
+end
 
+"""
+"""
+function vars(model::QUBOModel)::Vector{VirtualVar}
+    return model.varvec
+end
+
+"""
+"""
+function slackvars(model::QUBOModel)::Vector{VirtualVar}
+    return Vector{VirtualVar}([v for v in model.varvec if v.slack])
 end
