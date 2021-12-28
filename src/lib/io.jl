7ab2a908d4d0b0455bf17e786d7d3bc542d9a0b3
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2021-12-28 16:07:00 -0300

    Create io.jl

---
+++ src/lib/io.jl
@@ -0,0 +1,12 @@
+# -*- IO -*-
+function subscript(i::Int; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+    return join([var === nothing ? "" : var; par ? "₍" : "" ; i < 0 ? Char(0x208B) : ""; [Char(0x2080 + j) for j in reverse(digits(abs(i)))]; par ? "₎" : ""])
+end
+
+function subscript(i::Vector; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+    return join(subscript.(i, var=var, par=par), " ")
+end
+
+function subscript(i::Set; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+    return subscript(sort(collect(i)), var=var, par=par)
+end
