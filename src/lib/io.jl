179ff9ab066ebb58c3970c9c094e31d025dd9d1d
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2022-01-10 22:14:27 -0300

    Add JSON output

--- src/lib/io.jl
+++ src/lib/io.jl
@@ -1,25 +1,39 @@
 # -*- IO -*-
 
 # -*- Subscript: Generic -*-
-function subscript(i::Int; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+function subscript(i::Int; var::Union{Symbol, Nothing}=nothing, par::Bool=false)
     return join([var === nothing ? "" : var; par ? "₍" : "" ; i < 0 ? Char(0x208B) : ""; [Char(0x2080 + j) for j in reverse(digits(abs(i)))]; par ? "₎" : ""])
 end
 
-function subscript(v::Vector; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+function subscript(v::Vector; var::Union{Symbol, Nothing}=nothing, par::Bool=false)
     return join(subscript.(v, var=var, par=par), " ")
 end
 
-function subscript(s::Set; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+function subscript(s::Set; var::Union{Symbol, Nothing}=nothing, par::Bool=false)
     return subscript(sort(collect(s)), var=var, par=par)
 end
 
-function subscript(s::Symbol; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+function subscript(s::Symbol; var::Union{Symbol, Nothing}=nothing, par::Bool=false)
     return s
 end
 
 # -*- Subscript: MOI Stuff -*-
-function subscript(i::VI; var::Union{Symbol, Nothing}=nothing, par::Bool=false)::String
+function subscript(i::VI; var::Union{Symbol, Nothing}=nothing, par::Bool=false)
     return subscript(i.value; var=var, par=par)
+end
+
+# -*- Subscript: VirtualVar -*-
+function subscript(v::VV; var::Union{Symbol, Nothing}=nothing, par::Bool=false)
+    return subscript(v.source, var=v.var, par=isslack(v))
+end
+
+# -*- Show: VirtualVar -*-
+function Base.show(io::IO, v::VV)
+    if isslack(v)
+        print(io, var(v))
+    else
+        print(io, subscript(source(v), var=var(v)))
+    end
 end
 
 # -*- Show: PBF -*-
@@ -29,4 +43,28 @@ function Base.show(io::IO, p::PBO.PBF{S, T}) where {S, T}
     else
         print(io, join(join(["$((c < 0) ? (i == 1 ? "-" : " - ") : (i == 1 ? "" : " + "))$(abs(c)) $(subscript(t, var=:x))" for (i, (t, c)) in enumerate(p)])))
     end
+end
+
+# -*- JSON -*-
+function tojson(model::QUBOModel{T}) where T
+
+    terms = Dict{String, T}()
+
+    for (t, c) in model.E
+        if length(t) == 0
+            term = ""
+        elseif length(t) == 1
+            i, = t
+            term = "$(i.value) $(i.value)"
+        elseif length(t) == 2
+            i, j = t
+            term = "$(i.value) $(j.value)"
+        else
+            error("Invalid QUBO Model (degree >= 3)") 
+        end
+
+        terms[term] = c
+    end
+
+    return JSON.json(terms)
 end
---
+++ src/lib/moi.jl
@@ -0,0 +1,62 @@
+# ::: Input Model Support :::
+
+# -*- Objective Support -*-
+function supported_objective(model::MOI.ModelLike)
+    F = MOI.get(model, MOI.ObjectiveFunctionType())
+    if !__qubo_supported_objective(F)
+        error("Objective functions of type ", F, " are not implemented")
+    end
+    return
+end
+
+__qubo_supported_objective(::Type) = false
+__qubo_supported_objective(::Type{<: VI}) = true
+__qubo_supported_objective(::Type{<: SAF{T}}) where {T} = true
+__qubo_supported_objective(::Type{<: SQF{T}}) where {T} = true
+
+# -*- Constraint Support -*-
+function supported_constraints(model::MOI.ModelLike)
+    for (F, S) in MOI.get(model, MOI.ListOfConstraints())
+        if !__qubo_supported_constraint(F, S)
+            error(
+                "Constraints of function ",
+                F,
+                " in the Set ",
+                S,
+                " are not implemented",
+            )
+        end
+    end
+    return
+end
+
+__qubo_supported_constraint(::Type, ::Type) = false
+__qubo_supported_constraint(::Type{<: VI}, ::Type{<: ZO}) = true
+__qubo_supported_constraint(::Type{<: SAF{T}}, ::Type{<: EQ{T}}) where T = true
+__qubo_supported_constraint(::Type{<: SAF{T}}, ::Type{<: LT{T}}) where T = true
+__qubo_supported_constraint(::Type{<: SAF{T}}, ::Type{<: GT{T}}) where T = true
+
+# -*- Optimize! -*-
+function MOI.optimize!(model::QUBOModel)
+    if model.sampler === missing
+        error("QUBO Model 'sampler' is missing.")
+    end
+
+    x, Q, c = qubo(model.E₀ + model.Eᵢ)
+
+    sample!(model.sampler, x, Q, c)
+end
+
+# -*- Get: VariablePrimal -*-
+function MOI.get(model::QUBOModel{T}, ::MOI.VariablePrimal, xᵢ::MOI.VariableIndex) where {T}
+    return sum(
+        (MOI.get(model.model, MOI.VariablePrimal(), yᵢⱼ) * cᵢⱼ for (yᵢⱼ, cᵢⱼ) in model.source[xᵢ]);
+        init=zero(T)
+    )
+end
+
+# -*- The copy_to interface -*-
+function MOI.copy_to(sampler::AbstractSampler{V, S, T}, model::QUBOModel{T}) where {V, S, T}
+    ## MOI.copy_to(sampler, model.model)
+    error("NotImplementedError")
+end
