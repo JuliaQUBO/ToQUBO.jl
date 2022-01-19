9446c0aa2a87c0bcb4df35b80b6689248c9c6c89
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2022-01-19 18:11:31 -0300

    Add QUBO Error

--- src/lib/error.jl
+++ src/lib/error.jl
@@ -1,7 +1,16 @@
+# How to build custom errros:
+"""
 struct CustomError <: Exception
     msg::String
+    attr::String
 end
 
 function Base.showerror(io::IO, e::CustomError)
-    print(io, e.msg)
+    print(io, e.msg, e.attr)
+end
+"""
+
+
+struct QUBOError <: Exception
+    msg::String
 end
