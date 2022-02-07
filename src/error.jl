b13c834c51eb851a36634eb2a31e36ba2c539c5d
Pedro Maciel Xavier <pedromxavier@poli.ufrj.br>
2022-02-07 08:14:33 -0300

    Copy Error definition

--- src/error.jl
+++ src/error.jl
@@ -1,12 +1,20 @@
 """
-    QUBOError(msg::String)
+    QUBOError(msg::Union{Nothing, String})
 
 This error indicates any failure during QUBO formulation
 """
 struct QUBOError <: Exception
-    msg::String
+    msg::Union{Nothing, String}
+
+    function QUBOError(msg::Union{Nothing, String} = nothing)
+        return new(msg)
+    end
 end
 
 function Base.showerror(io::IO, e::QUBOError)
-    print(io, e.msg)
+    if e.msg === nothing
+        print(io, "The current model could not be converted to QUBO")
+    else
+        print(io, e.msg)
+    end
 end
