007cda524325a87e90b4e890a0b5d58933380de9
pedromxavier <pedromxavier@poli.ufrj.br>
2022-02-22 18:52:26 -0300

    Extend PyCall Interface

--- src/Anneal.jl/src/pyimport.jl
+++ src/Anneal.jl/src/pyimport.jl
@@ -1,42 +1,39 @@
+using PyCall
+using Conda
+
 """
-    @python_import(name, module_name::String, package_name::String)
+    python_import(name, module_name::String, package_name::String)
 
 Wrapper for pyimport_conda with support to PyPi-only packages.
 
 ## Example
-julia> @python_import neal "neal" "dwave-neal"
+julia> const neal = python_import("neal", "dwave-neal")
 """
-macro python_import(name, module_name::String, package_name::String)
-    return :(
-        const $name = PyNULL();
+function python_import(module_name::String, package_name::String)
+    try
+        return pyimport_conda(module_name, package_name)
+    catch
+        if PyCall.conda
+            @warn "'$module_name' is not installed.\nRunning `pip install --user $package_name` (via conda)"
 
-        function __init__();
-            try;
-                copy!($name, pyimport_conda($module_name, $package_name));
-            catch;
-                if PyCall.conda;
-                    @warn string("'", $module_name, "' is not installed.\n", "Running `pip install --user ", $package_name, "` (via conda)");
+            try
+                Conda.pip_interop(true)
+                Conda.pip("install --user", "$package_name")
+            catch
+                throw(SystemError("Unable to install '$package_name' using pip (via conda)"))
+            end
+        else
+            @warn "'$module_name' is not installed.\nRunning `$(PyCall.python) -m pip install --user $package_name`"
 
-                    try;
-                        Conda.pip_interop(true);
-                        Conda.pip("install --user", "$package_name");
-                    catch;
-                        throw(SystemError("Unable to install '", $package_name, "' using pip (via conda)"));
-                    end;
-                else;
-                    @warn string("'", $module_name, "' is not installed.\n", "Running `$(PyCall.python) -m pip install --user ", $package_name, "`");
+            cmd = Cmd([PyCall.python, "-m", "pip", "install", "--user", package_name])
+            
+            ans = run(cmd)
 
-                    cmd = Cmd([PyCall.python, "-m", "pip", "install", "--user", $package_name]);
-                    
-                    ans = run(cmd);
-
-                    if ans.exitcode != 0;
-                        throw(SystemError("Unable to install '", $package_name, "' using pip", ans.exitcode));
-                    end
-                end;
-
-                copy!($name, pyimport($module_name));
-            end;
+            if ans.exitcode != 0
+                throw(SystemError("Unable to install '$package_name' using pip", ans.exitcode))
+            end
         end;
-    )
+
+        return pyimport(module_name)
+    end
 end
