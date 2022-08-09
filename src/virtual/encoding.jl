1b19f6b7d384c4cf185871f3fd90afaace6c5645
pedromxavier <pedromxavier@poli.ufrj.br>
2022-08-09 02:24:22 -0300

    Continue encoding development

--- src/virtual/encoding.jl
+++ src/virtual/encoding.jl
@@ -1,3 +1,107 @@
+abstract type LinearEncoding <: Encoding end
+
+@doc raw"""
+""" struct Mirror <: LinearEncoding end
+@doc raw"""
+""" struct Linear <: LinearEncoding end
+@doc raw"""
+""" struct Unary <: LinearEncoding end
+
+@doc raw"""
+Binary Expansion within the closed interval ``[\alpha, \beta]``.
+
+For a given variable ``x \in [\alpha, \beta]`` we approximate it by
+
+```math    
+x \approx \alpha + \frac{(\beta - \alpha)}{2^{n} - 1} \sum_{i=0}^{n-1} {2^{i}\, y_i}
+```
+
+where ``n`` is the number of bits and ``y_i \in \mathbb{B}``.
+""" struct Binary <: LinearEncoding end
+
+function VirtualVariable{E, T}(
+        x::Union{VI, Nothing},
+        y::Vector{VI},
+        γ::Vector{T},
+        α::T = zero(T),
+    ) where {E <: LinearEncoding, T}
+    @assert (n = length(y)) == length(γ)
+    
+    VirtualVariable{E, T}(
+        x,
+        y,
+        (α + PBO.PBF{VI, T}(y[i] => γ[i] for i = 1:n)),
+        nothing,
+    )
+end
+
+@doc raw"""
+""" struct OneHot <: LinearEncoding end
+
+function VirtualVariable{OneHot, T}(
+        x::Union{VI, Nothing},
+        y::Vector{VI},
+        γ::Vector{T},
+        α::T = zero(T),
+    ) where T
+    @assert (n = length(y)) == length(γ)
+
+    VirtualVariable{OneHot, T}(
+        x,
+        y,
+        (α + PBO.PBF{VI, T}(y[i] => γ[i] for i = 1:n)),
+        (one(T) - PBO.PBF{VI, T}(y)) ^ 2,
+    )
+end
+
+abstract type SequentialEncoding <: Encoding end
+
+struct DomainWall <: SequentialEncoding end
+
+function VirtualVariable{DomainWall, T}(
+        x::Union{VI, Nothing},
+        y::Vector{VI},
+        γ::Vector{T},
+        α::T = zero(T),
+    ) where T
+    @assert (n = length(y)) == length(γ) - 1
+
+    ξ = PBO.PBF{VI, T}(y[i] => (γ[i] - γ[i+1]) for i = 1:n)
+    h = 2.0 * (PBO.PBF{VI, T}(y[2:n]) - PBO.PBF{VI, T}([Set{VI}([y[i], y[i-1]]) for i = 2:n]))
+
+    VirtualVariable{DomainWall, T}(x, y, ξ, h)
+end
+
+abstract type ConstantEncoding <: Encoding end
+
+function VirtualVariable{ConstantEncoding, T}(x::Union{VI, Nothing}, β::T)
+    VirtualVariable{ConstantEncoding, T}(
+        x,
+        VI[],
+        PBO.PBF{VI, T}(β),
+        nothing,
+    )
+end
+
+abstract type PartialEncoding <: Encoding end
+
+struct BipartiteEncoding{A <: Encoding, B <: Encoding} <: PartialEncoding end
+
+function VirtualVariable{BipartiteEncoding{A, B}, T}(
+        u::VirtualVariable{A, T},
+        v::VirtualVariable{B, T},
+        z::VI,
+    ) where {A <: Encoding, B <: Encoding, T}
+    @assert source(u) === source(v)
+
+    VirtualVariable{BipartiteEncoding{A, B}, T}(
+        source(u),
+        VI[target(u); target(v)],
+        PBO.PBF{VI, T}(nothing, z => -1.0) * expansion(v) + PBO.PBF{VI, T}(z) * expansion(u),
+        penaltyfn(u) + penaltyfn(v),
+    )
+end
+
 @doc raw"""
     encode!(model::AbstractVirtualModel{T}, v::VirtualVariable{T}) where {T}
 
@@ -9,10 +113,11 @@ Maps newly created virtual variable `v` within the virtual model structure. It f
 """
 function encode! end
 
-function encode!(model::AbstractVirtualModel{T}, v::VV{<:Any, T}) where T
+function encode!(model::AbstractVirtualModel{T}, v::VirtualVariable{<:Any, T}) where T
     if !isslack(v)
-        x = source(v)
-        MOI.set(model, Source(), x, v)
+        let x = source(v)
+            MOI.set(model, Source(), x, v)
+        end
     end
 
     for y in target(v)
@@ -135,8 +240,16 @@ function encode!(E::Type{<:DomainWall}, model::AbstractVirtualModel{T}, x::Union
     encode!(E, model, x, α .+ T[i for i = 0:M], zero(T))
 end
 
-
 function encode!(E::Type{<:DomainWall}, model::AbstractVirtualModel{T}, x::Union{VI, Nothing}, a::T, b::T, n::Integer) where T
     Γ = (b - a) / (n - 1)
     encode!(E, model, x, a .+ Γ * collect(T, 0:n-1), zero(T))
+end
+
+function encode!(
+    E::Type{<:BipartiteEncoding},
+    model::AbstractVirtualModel{T},
+    x::Union{VI, Nothing},
+    
+    )
+
 end
