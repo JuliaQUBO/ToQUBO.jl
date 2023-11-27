b1a3ebfce830da746685c56de9485ebfbc28d3b4
pedromxavier <pedromxavier@poli.ufrj.br>
2023-11-27 00:40:14 -0500

    Add steup callback hook

--- src/compiler/setup.jl
+++ src/compiler/setup.jl
@@ -1,8 +1,15 @@
-function setup!(model::Virtual.Model, arch::AbstractArchitecture)
-    level = MOI.get(model, Attributes.Optimization())
+function setup!(model::Virtual.Model, ::AbstractArchitecture)
+    # level = MOI.get(model, Attributes.Optimization())
 
-    if level >= 1
-                
+    # if level >= 1
+    #
+    # [Control other settings here]
+    #
+    # end
+
+    # Call setup callback
+    let setup_callback! = get(model.compiler_settings, :setup_callback, identity)
+        setup_callback!(model)
     end
 
     return nothing
