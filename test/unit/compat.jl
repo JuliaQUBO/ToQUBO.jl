a547a6d228358382980e6cfb611b0c2fd28c257d
David E. Bernal Neira <dbernaln@purdue.edu>
2026-06-19 09:51:15 -0400

    test: accept docs dependency compat bump

--- test/unit/compat.jl
+++ test/unit/compat.jl
@@ -12,8 +12,9 @@ function test_compat()
         @test occursin(r"(^|,\s*)0\.11(\s*(,|$))", compat["QUBOTools"])
         @test occursin(r"(^|,\s*)0\.12(\s*(,|$))", compat["QUBOTools"])
         @test occursin(r"(^|,\s*)0\.13(\s*(,|$))", compat["QUBOTools"])
-        @test docs_compat["PySA"] == "0.3.4"
+        @test docs_compat["PySA"] == "0.3.4, 0.4"
         @test occursin(r"(^|,\s*)0\.4(\s*(,|$))", docs_compat["QUBODrivers"])
+        @test occursin(r"(^|,\s*)0\.6(\s*(,|$))", docs_compat["QUBODrivers"])
         @test docs_compat["QUBOTools"] == "0.12, 0.13"
         @test docs_compat["ToQUBO"] == "0.4.1"
         @test occursin(r"(^|,\s*)0\.11(\s*(,|$))", test_compat["QUBOTools"])
